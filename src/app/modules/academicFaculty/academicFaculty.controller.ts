fee5503d168c6370c912cf02a76faea6c6ae5a91
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-07-07 02:33:13 +0600

    pagination started

--- src/app/modules/academicFaculty/academicFaculty.controller.ts
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
 import catchAsync from '../../../shared/catchAsync';
+import { pick } from '../../../shared/pick';
 import sendReponse from '../../../shared/sendResponse';
 import {
   createFacultyToDB,
@@ -21,12 +23,15 @@ export const createFaculty = catchAsync(async (req: Request, res: Response) => {
 });
 
 export const getAllFaculty = catchAsync(async (req: Request, res: Response) => {
-  const result = await getAllFacultyToDB();
+  const paginationOptions = pick(req.query, paginationFields);
+
+  const result = await getAllFacultyToDB(paginationOptions);
 
   sendReponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Faculty retrieved successfully !',
-    data: result,
+    meta: result.meta,
+    data: result.data,
   });
 });
--- src/app/modules/academicFaculty/academicFaculty.service.ts
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -1,3 +1,5 @@
+import { calculatePagination } from '../../../helpers/paginationHelper';
+import { IPaginationOptions } from '../../../interfaces/pagination';
 import { IAcademicFaculty } from './academicFaculty.interface';
 import { AcademicFaculty } from './academicFaculty.model';
 
@@ -8,8 +10,22 @@ export const createFacultyToDB = async (
   return result;
 };
 
-export const getAllFacultyToDB = async () => {
-  const result = await AcademicFaculty.find();
+export const getAllFacultyToDB = async (
+  paginationOptions: IPaginationOptions
+) => {
+  const { page, limit, skip, sortBy, sortOrder } =
+    calculatePagination(paginationOptions);
 
-  return result;
+  const result = await AcademicFaculty.find().skip(skip).limit(limit);
+
+  const total = await AcademicFaculty.countDocuments();
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
 };
