d7f941ca0992d2e7535fa58793d6c20d37eec39c
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-07-08 16:29:53 +0600

    add vartual in model

--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -9,21 +9,10 @@ const academicFacultySchema = new Schema(
   },
   {
     timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
   }
 );
-
-// academicSemesterSchema.pre('save', async function (next) {
-//   const isExist = await AcademicSemester.findOne({
-//     title: this.title,
-//     year: this.year,
-//   });
-//   if (isExist) {
-//     throw new ApiError(
-//       httpStatus.CONFLICT,
-//       'Academic semester is already exist !'
-//     );
-//   }
-//   next();
-// });
 
 export const AcademicFaculty = model('AcademicFaculty', academicFacultySchema);
