ce05ff41c08e0c4a2456efa1465a098d6efa7e5e
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-07-17 16:40:38 +0600

    creating auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,38 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status';
-import { Secret } from 'jsonwebtoken';
-import config from '../../config';
-import ApiError from '../../errors/ApiError';
-import { jwtHelpers } from '../../helpers/jwtHelpers';
 
-interface CustomRequest extends Request {
-  user?: any;
-}
-
-const auth =
-  (...requiredRoles: string[]) =>
-  async (req: CustomRequest, res: Response, next: NextFunction) => {
+export const auth =
+  () => async (req: Request, res: Response, next: NextFunction) => {
     try {
-      //get authorization token
-      const token = req.headers.authorization;
-      if (!token) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, 'You are not authorized');
-      }
-      // verify token
-      let verifiedUser = null;
-
-      verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret);
-
-      req.user = verifiedUser; // role  , userid
-
-      // role diye guard korar jnno
-      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
-        throw new ApiError(httpStatus.FORBIDDEN, 'Forbidden');
-      }
-      next();
     } catch (error) {
       next(error);
     }
   };
-
-export default auth;
--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import { auth } from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import {
   createFaculty,
@@ -16,15 +18,43 @@ const router = express.Router();
 router.post(
   '/create-faculty',
   validateRequest(createAcademicFacultyZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   createFaculty
 );
-router.get('/:id', getSingleFaculty);
-router.get('/', getAllFaculty);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  getSingleFaculty
+);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  getAllFaculty
+);
 router.patch(
   '/update-faculty/:id',
   validateRequest(updateAcademicFacultyZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY
+  ),
   updateFaculty
 );
-router.delete('/:id', deleteFaculty);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  deleteFaculty
+);
 
 export const AcademicFacultyRoutes = router;
